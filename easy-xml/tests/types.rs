12fe812416897fcf970a3b0ed8bf94776c5be5d2
qaqtutu <203524203@qq.com>
2021-10-17 23:13:00 +0800

    添加Option在错误情况下的测试

--- easy-xml/tests/types.rs
+++ easy-xml/tests/types.rs
@@ -29,3 +29,18 @@ fn test_for_option() {
     );
     assert_eq!(node, de::from_str::<Node>(xml.as_str()).unwrap());
 }
+#[test]
+fn test_for_option_with_err() {
+    #[derive(PartialEq, Debug, XmlDeserialize, XmlSerialize)]
+    struct Node {
+        #[easy_xml(text)]
+        test: Option<i32>,
+    }
+    let xml = r#"<Node>123</Node>"#;
+    let node = de::from_str::<Node>(xml).unwrap();
+    assert_eq!(node.test, Some(123));
+
+    let xml = r#"<Node>err</Node>"#;
+    let node = de::from_str::<Node>(xml).unwrap();
+    assert_eq!(node.test, None)
+}
